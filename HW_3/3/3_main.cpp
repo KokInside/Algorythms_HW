fccb4e3f3f83bb77838332ff0283c7ed466a0027
KokInside <ivankhvostov2005@gmail.com>
2025-12-31 03:52:02 +0300

    HW 3, task 3

---
+++ HW_3/3/3_main.cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+#include <vector>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <map>
+#include <set>
+#include <cstdint>
+#include <algorithm>
+
+class IGraph {
+public:
+
+	virtual ~IGraph() {}
+
+	// Добавление ребра от from к to.
+	virtual void AddEdge(int from, int to, int weight) = 0;
+
+	virtual int VerticesCount() const = 0;
+
+	virtual std::vector<std::pair<int, int>> GetNextVertices(int vertex) const = 0;
+
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Реализация Графа
+
+class ListGraph : public IGraph {
+public:
+
+	ListGraph(size_t size);
+
+	ListGraph(const IGraph& graph);
+
+	void AddEdge(int from, int to, int weight) override;
+
+	int VerticesCount() const override;
+
+	std::vector<std::pair<int, int>> GetNextVertices(int vertex) const override;
+
+	~ListGraph();
+
+private:
+
+	std::vector<std::vector<std::pair<int, int>>> adjacencyList;
+};
+
+ListGraph::ListGraph(size_t size) : adjacencyList(size) {
+}
+
+void ListGraph::AddEdge(int from, int to, int weight) {
+
+	if (from < 0 || to < 0 || from >= VerticesCount() || to >= VerticesCount()) {
+		throw std::out_of_range("Out of vertices range");
+	}
+
+	if (from == to) {
+		return;
+	}
+
+	auto it = std::find_if(adjacencyList[from].begin(), adjacencyList[from].end(), [&to](const std::pair<int, int>& p) {return p.first == to; });
+
+	// Если ребро есть и оно легче - обновляем
+	if (it != adjacencyList[from].end()) {
+		if (weight < it->second) {
+			it->second = weight;
+		}
+	}
+	else {
+		adjacencyList[from].push_back(std::pair<int, int>(to, weight));
+	}
+
+	// Если есть обратное ребро и оно легче - обновляем
+	if (from != to) {
+		auto it = std::find_if(adjacencyList[to].begin(), adjacencyList[to].end(), [&from](const std::pair<int, int>& p) {return p.first == from; });
+		
+		if (it != adjacencyList[to].end()) {
+			if (weight < it->second) {
+				it->second = weight;
+			}
+		}
+		else {
+			adjacencyList[to].push_back(std::pair<int, int>(from, weight));
+		}
+	}
+}
+
+int ListGraph::VerticesCount() const {
+
+	return static_cast<int>(adjacencyList.size());
+}
+
+std::vector<std::pair<int, int>> ListGraph::GetNextVertices(int vertex) const {
+
+	if (vertex < 0 || vertex >= VerticesCount()) {
+		throw std::out_of_range("Out of vertices range");
+	}
+
+	return adjacencyList[vertex];
+}
+
+ListGraph::~ListGraph() {
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Дейкстра
+
+size_t Dijkstra(const IGraph& graph, int from, int to) {
+
+	if (from < 0 || to < 0 || from >= graph.VerticesCount() || to >= graph.VerticesCount()) {
+		throw std::out_of_range("Out of vertices range");
+	}
+
+	std::vector<size_t> minPath(graph.VerticesCount(), UINT64_MAX);
+
+	minPath[from] = 0;
+
+	std::set<std::pair<size_t, size_t>> s;
+
+	s.insert(std::pair<size_t, size_t>(0, from));
+
+	while (!s.empty()) {
+		size_t min_path = (*(s.begin())).first;
+		size_t vertex = (*(s.begin())).second;
+
+		s.erase(std::pair<size_t, size_t>(min_path, vertex));
+
+		std::vector<std::pair<int, int>> nextVertices = graph.GetNextVertices(vertex);
+
+		for (int i = 0; i < nextVertices.size(); ++i) {
+
+			int path = nextVertices[i].second;
+			int ver = nextVertices[i].first;
+
+			if (minPath[vertex] != UINT64_MAX && minPath[ver] > minPath[vertex] + path) {
+				s.erase({ minPath[ver], ver });
+				minPath[ver] = minPath[vertex] + path;
+				s.insert({ minPath[ver], ver });
+			}
+		}
+	}
+
+	return minPath[to];
+}
+
+int main() {
+
+	int N{}, M{};
+
+	std::cin >> N >> M;
+
+	ListGraph graph(N);
+
+	for (int i = 0; i < M; ++i) {
+		int u{}, v{}, w{};
+
+		std::cin >> u >> v >> w;
+
+		graph.AddEdge(u, v, w);
+	}
+
+	int s{}, t{};
+	
+	std::cin >> s >> t;
+
+	std::cout << Dijkstra(graph, s, t);
+
+	return 0;
+}
